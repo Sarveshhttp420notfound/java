a4267d7fd18f2abb92a6354d8ae9f22d6bdf4315
Sarvesh kumar singh <87657797+Sarveshhttp420notfound@users.noreply.github.com>
2025-04-11 23:39:36 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,12 @@
+class Program
+{
+   public static void main(String[] args)
+   {
+
+         System.out.println("Hi");
+         System.out.println(10+20);
+         System.out.println(10.0/20);
+         System.out.println(10+20*3);
+}
+
+}
