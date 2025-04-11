6f53c1eeb562e93a63d7c19ef3a513674222f313
Sarvesh kumar singh <87657797+Sarveshhttp420notfound@users.noreply.github.com>
2025-04-11 23:39:19 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,15 @@
+class Student
+{
+   public static void main(String[] args)
+   {
+
+         System.out.println("Name:-Sarvesh");
+         System.out.println("Age:- 24");
+         System.out.println("mobile no:-9854785847");
+         System.out.println("Email:-s@gmail.com");
+         System.out.println("Address:-Noida");
+
+
+}
+
+}
